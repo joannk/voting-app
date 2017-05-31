83374674060671cadbd47a4edf39029ca558d9e0
Joann Kim <jsk031@gmail.com>
2017-05-30 21:29:56 -0400

    Render multiple products using seed data

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,8 @@
 class ProductList extends React.Component {
   render() {
-    const product = Seed.products[0];
-    return (
-      <div className='ui stackable items'>
+    const productComponents = Seed.products.map((product) => (
         <Product
+          key={'product-' + product.id}
           id={product.id}
           title={product.title}
           description={product.description}
@@ -12,6 +11,11 @@ class ProductList extends React.Component {
           submitterAvatarUrl={product.submitterAvatarUrl}
           productImageUrl={product.productImageUrl}
         />
+    ));
+
+    return (
+      <div className='ui stackable items'>
+        {productComponents}
       </div>
     );
   }
